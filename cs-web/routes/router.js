b0eb88be8f2a51e4b28e88e2a1a10d1aa7b05753
marpio <marcinkowski@mailbox.org>
2019-12-21 22:13:18 +0100

    add missing contract sections

--- cs-web/routes/router.js
+++ cs-web/routes/router.js
@@ -26,60 +26,62 @@ module.exports = (bookingSrv, invoiceSrv, timeSlotsSrv, personSrv) => {
     '/:id/contract',
     asyncMiddleware(async (req, res, next) => {
       const b = await bookingSrv.get(req.params.id);
-      const p = await personSrv.getById(b.get('Mieter'));
-      const ts = await timeSlotsSrv.getBookingTimeSlots(b.get('Key'));
-      const invoiceItems = await invoiceSrv.getInvoceItemsByBooking(
-        b.get('Key')
-      );
+      if (b.get('Status') !== 'Vorreserviert') {
+        res.status(403);
+      } else {
+        const p = await personSrv.getById(b.get('Mieter'));
+        const ts = await timeSlotsSrv.getBookingTimeSlots(b.get('Key'));
+        const invoiceItems = await invoiceSrv.getInvoceItemsByBooking(
+          b.get('Key')
+        );
 
-      const contract = {
-        title: b.get('Titel'),
-        participantsCount: b.get('TeilnehmerInnenanzahl'),
-        person: {
-          name: p.get('Vorname') + ' ' + p.get('Nachname'),
-          telefon: p.get('Tel'),
-          email: p.get('Email'),
-          org: p.get('Organisation'),
-          address:
-            p.get('Strasse') + ' ' + p.get('HausNr') + '/' + p.get('Top'),
-          postCode: p.get('PLZ'),
-          city: p.get('Ort'),
-          uid: p.get('UID'),
-          umsatzsteuerbefreit: p.get('Umsatzsteuerbefreit')
-        },
-        timeSlots: ts
-          .map(t => {
+        const contract = {
+          title: b.get('Titel'),
+          participantsCount: b.get('TeilnehmerInnenanzahl'),
+          person: {
+            name: p.get('Vorname') + ' ' + p.get('Nachname'),
+            telefon: p.get('Tel'),
+            email: p.get('Email'),
+            org: p.get('Organisation'),
+            address:
+              p.get('Strasse') + ' ' + p.get('HausNr') + '/' + p.get('Top'),
+            postCode: p.get('PLZ'),
+            city: p.get('Ort'),
+            uid: p.get('UID'),
+            umsatzsteuerbefreit: p.get('Umsatzsteuerbefreit')
+          },
+          timeSlots: ts
+            .map(t => {
+              return {
+                room: t.get('RaumName')[0],
+                type: t.get('Type'),
+                beginn: moment(t.get('Beginn')),
+                end: moment(t.get('Ende'))
+              };
+            })
+            .filter(t => t.type !== 'Reinigung')
+            .sort((a, b) => (a.beginn.isAfter(b.beginn) ? 1 : -1))
+            .map(t => {
+              return {
+                room: t.room,
+                type: t.type,
+                beginn: t.beginn.format('DD.MM.YYYY hh:mm'),
+                end: t.end.format('DD.MM.YYYY hh:mm')
+              };
+            }),
+          invoiceItems: invoiceItems.map(e => {
             return {
-              room: t.get('RaumName')[0],
-              type: t.get('Type'),
-              beginn: moment(t.get('Beginn')),
-              end: moment(t.get('Ende'))
-            };
-          })
-          .filter(t => t.type !== 'Reinigung')
-          .sort((a, b) => (a.beginn.isAfter(b.beginn) ? 1 : -1))
-          .map(t => {
-            return {
-              room: t.room,
-              type: t.type,
-              beginn: t.beginn.format('DD.MM.YYYY hh:mm'),
-              end: t.end.format('DD.MM.YYYY hh:mm')
+              name: e.get('ArtikelName'),
+              showCount: e.get('ArtikelTyp')[0] === 'Ausstattung',
+              count: e.get('Anzahl'),
+              price: e.get('SummeNetto')
             };
           }),
-        invoiceItems: invoiceItems.map(e => {
-          return {
-            name: e.get('ArtikelName'),
-            showCount: e.get('ArtikelTyp')[0] === 'Ausstattung',
-            count: e.get('Anzahl'),
-            price: e.get('SummeNetto')
-          };
-        }),
-        notes: b.get('Notes')
-      };
+          notes: b.get('Notes')
+        };
 
-      console.log(JSON.stringify(contract));
-
-      res.render('contract', contract);
+        res.render('contract', contract);
+      }
     })
   );
 
