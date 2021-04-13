c62591faf6d4b9306c71ab70f4a8a6a42d096c1b
marpio <marcinkowski@mailbox.org>
2021-04-13 22:48:34 +0200

    feedback

--- cs-web/app.js
+++ cs-web/app.js
@@ -56,22 +56,4 @@ app.use(
   bookingsRouter(bookingSrv, invoiceSrv, timeslotsSrv, personSrv)
 );
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  //console.error(err.)
-  console.error(err.stack);
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
