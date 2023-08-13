73448a116bf81843d1f9d100598ff5dd6eb94713
marpio <marcinkowski@mailbox.org>
2023-08-13 22:21:14 +0200

    add 'ohne moeblierung' option

--- src/public/javascripts/booking.js
+++ src/public/javascripts/booking.js
@@ -14,7 +14,7 @@ Vue.component('booking-form', {
         notes: '',
         timeSlots: []
       },
-      moeblierungen: ['Block', 'Cafe', 'Carre', 'Geschwungene-Reihen', 'Kreis', 'U-Form', 'Kino Bestuhlung', 'Andere'],
+      moeblierungen: ['Ohne Möblierung', 'Block', 'Cafe', 'Carre', 'Geschwungene-Reihen', 'Kreis', 'U-Form', 'Kino Bestuhlung', 'Andere'],
       rooms: [],
       equipment: [],
       hours: [
