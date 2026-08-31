9a1a6af91043ac57e7cae3d8044511823fe8a288
marpio <piotr@marcinkowski.be>
2026-08-31 16:56:09 +0200

    cleanup

--- start-dev.sh
+++ start-dev.sh
@@ -3,7 +3,7 @@
 caddy stop
 XDG_DATA_HOME=./caddy_data XDG_CONFIG_HOME=./caddy_config caddy start --config Caddyfile-dev
 
-(cd frontend && npm install && npm run build)
+(rm -rf src/public/assets && rm src/public/booking-*.html && cd frontend && npm install && npm run build)
 rm -rf dist/ && \
 tsc && \
 cp -R src/public dist/ && \
